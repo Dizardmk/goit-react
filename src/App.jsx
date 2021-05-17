b83064b701dabc30d9f0f6cbd612ec35ada36512
Dizardmk <Dizardmk@gmail.com>
2021-05-17 13:36:07 +0300

    adds hw6-redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import Section from './components/Section';
 import ContactEditor from './components/ContactEditor';
 import ContactFilter from './components/ContactFilter';
 import ContactList from './components/ContactList';
 
-class App extends Component {
-  // componentDidMount() {
-  //   if (JSON.parse(localStorage.getItem('contacts'))) {
-  //     this.setState({ contacts: JSON.parse(localStorage.getItem('contacts')) });
-  //   }
-  // }
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (this.state.contacts !== prevState.contacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  //   }
-  // }
-
-  render() {
-    const { contacts } = this.props;
-
-    return (
-      <>
-        <Section title="Phonebook">
-          <ContactEditor />
-        </Section>
-        <Section title="Contacts">
-          {contacts.length > 0 && <ContactFilter />}
-          <ContactList />
-        </Section>
-      </>
-    );
-  }
-}
+const App = ({ contacts }) => (
+  <>
+    <Section title="Phonebook">
+      <ContactEditor />
+    </Section>
+    <Section title="Contacts">
+      {contacts.length > 0 && <ContactFilter />}
+      <ContactList />
+    </Section>
+  </>
+);
 
 const mapStateToProps = state => ({
   contacts: state.contacts.items,
--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -2,26 +2,41 @@ import { combineReducers } from 'redux';
 import types from './contacts-types';
 
 const items = (state = [], { type, payload }) => {
-  switch (type) {
-    case types.ADD:
-      return [...state, payload];
-
-    case types.DELETE:
-      return state.filter(({ id }) => id !== payload);
-
-    default:
+  ///////////////////////// ADD
+  if (type === types.ADD) {
+    const addContactFilter = state.find(
+      ({ name }) => name.toLowerCase() === payload.name.toLowerCase(),
+    );
+    if (addContactFilter) {
+      alert(`${payload.name} is already in contacts.`);
       return state;
+    }
+    return [...state, payload];
   }
+  ///////////////////////// DELETE
+  if (type === types.DELETE) {
+    return state.filter(({ id }) => id !== payload);
+  }
+  ///////////////////////// DEFAULT
+  return state;
+
+  // switch (type) {
+  //   case types.ADD: return [...state, payload];
+  //   case types.DELETE: return state.filter(({ id }) => id !== payload);
+  //   default: return state;
+  // }
 };
 
 const filter = (state = '', { type, payload }) => {
-  switch (type) {
-    case types.CHANGE_FILTER:
-      return payload;
+  return type === types.CHANGE_FILTER ? payload : state;
 
-    default:
-      return state;
-  }
+  // if (type === types.CHANGE_FILTER) {return payload;}
+  // return state;
+
+  // switch (type) {
+  //   case types.CHANGE_FILTER: return payload;
+  //   default: return state;
+  // }
 };
 
 export default combineReducers({
